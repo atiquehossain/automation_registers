970e23e25e96b9051391bf5ad2d47cffb0e67eaa
atique <nsuatique@gmail.com>
2025-02-04 09:20:59 +0600

    project structure

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api_base_url = "base_url";
+
+
+//endpoints
+const ENDPOINTS = {
+    foruploadexcel : "/foruploadexcel/"
+};
+
+// function to get data 
+
+export const fetchData = async () => {
+    try {
+        const response =  await axios.get('${api_base_url}${foruploadexcel}')
+        return response.data;
+    } catch(error){
+        console.error("Error fetch excel data: ",error);
+        return [];
+    }
+    
+};
+
